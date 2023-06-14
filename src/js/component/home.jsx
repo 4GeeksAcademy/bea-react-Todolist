390285460acb506853085b349a00247619981250
beanama <129529299+beanama@users.noreply.github.com>
2023-06-14 18:56:09 +0000

    todo list with objects

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
 		return(
 
-			<TodoListWithArray />
+			<TodoListWithArrayOfObjects />
 
 		)
 			
@@ -55,6 +55,69 @@ const TodoListWithArray = () => {
 					return (
 						<section key={index} style={{display:"flex", padding:"0.1rem"}}>
 							<p className="todoItem">{`${todoItem}`}</p>
+							<button className="closeX" onClick={() => deleteElementOnClick(index)}>X</button>
+						</section>
+					)
+				})
+
+			}
+
+		</div>
+	</>
+}
+
+const TodoListWithArrayOfObjects = () => {
+
+	const defaultState = [
+
+		{
+			title: 'Hola',
+			description: 'Description',
+		}
+
+	]
+
+	const [todoList, setTodoList] = React.useState(defaultState)
+	const [inputValue, setInputValue] = React.useState("")
+	const ADD_NEW_VALUE_KEY = "Enter"
+
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value)
+	}
+
+	const addNewElement = () => {
+		if(!inputValue) return
+
+		setTodoList(prev => [...prev, {title: inputValue, description: inputValue + 'description'}])
+		setInputValue("")
+	}
+
+	const handleClick = () => addNewElement()
+
+	const onEnter = (e) => {
+		if (e.key === ADD_NEW_VALUE_KEY) addNewElement()
+	}
+
+	const deleteElementOnClick = (indexToDelete) => {
+		setTodoList(prev => prev.filter((_, index) => index !== indexToDelete))
+	}
+
+	return <>
+
+		<h1>To do List</h1>
+
+		<div className="input">
+
+			<input value={inputValue} onChange={handleInputChange} onKeyDown={onEnter} />
+
+			<button onClick={handleClick}>add one</button>
+
+			{
+				todoList.map((todoItem, index) => {
+					return (
+						<section key={index} style={{display:"flex", padding:"0.1rem"}}>
+							<h5 className="titleObject">{todoItem.title}</h5>
+							<p className="todoItem">{todoItem.description}</p>
 							<button className="closeX" onClick={() => deleteElementOnClick(index)}>X</button>
 						</section>
 					)
